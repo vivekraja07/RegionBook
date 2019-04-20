c9f9d7427a3e2e42561aab14390228c5aa61f386
vivekraja07 <vivekraja07@gmail.com>
2019-04-20 15:48:32 -0400

    two-way binding between select and map

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var formatComma = d3.format(',')
 var formatDecimal = d3.format(".1f")
 
+var selectValue = ''
+
+
 d3.json("topo.json", function (error, us) {
   if (error) throw error;
 
@@ -12,13 +15,23 @@ d3.json("topo.json", function (error, us) {
 
   // console.log(features)
 
+  var regionSelect = d3.select('#regionSelect')
+  var regionSelect_html = document.getElementById('regionSelect');
+
+
   d3.csv("colleges.csv", function (csv) {
+
+    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////// Data Aggregation ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    /* #region Main */
+
 
     var populationByRegion = d3.nest()
       .key(function (d) { return d.Region; })
       .rollup(function (v) { return d3.sum(v, function (d) { return d['Undergrad Population']; }); })
       .entries(csv);
-    
+
     var popObject = d3.nest()
       .key(function (d) { return d.Region; })
       .rollup(function (v) { return d3.sum(v, function (d) { return d['Undergrad Population']; }); })
@@ -52,14 +65,18 @@ d3.json("topo.json", function (error, us) {
     }
     // console.log(us)
 
+    /* #endregion */
+
+
     ///////////////////////////////////////////////////////////////////////////
     ////////////////////////// Draw the Map ////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
+    // #region
     var color = d3.scaleLinear()
-  // .domain([310575, 1893765]) 
-  .domain([200000, 1900000])
-  .range(["white", "#8B008B"])
+      // .domain([310575, 1893765]) 
+      .domain([200000, 1900000])
+      .range(["white", "#8B008B"])
 
 
     svg.append("g")
@@ -70,6 +87,10 @@ d3.json("topo.json", function (error, us) {
       .attr("d", path)
       .attr('fill', function (d) {
         return color(d.properties.pop);
+      })
+      .attr("id", function(d) {
+        var trim = d.properties.regionName.replace(/\s/g, "");
+        return trim
       })
       .on("mouseover", function (d) {
 
@@ -86,8 +107,8 @@ d3.json("topo.json", function (error, us) {
           .duration(500)
           .style("opacity", 0);
       })
-      
-
+      .on('click', selected)
+          
 
     svg.append("path")
       .attr("class", "region-borders")
@@ -98,9 +119,33 @@ d3.json("topo.json", function (error, us) {
       .attr("class", "tooltip")
       .style("opacity", 0);
 
+    function selected(d) {
+      d3.select('.selectedRegion').classed('selectedRegion', false);
+      d3.select(this).classed('selectedRegion', true);
+
+
+
+      //need to access the regionName from 
+        // console.log(d.properties.regionName)
+        for (var i=0; i<regionSelect_html.options.length; i++){
+          // console.log(regionSelect_html.options[i].value)
+          if (regionSelect_html.options[i].value == d.properties.regionName){
+            // console.log(regionSelect_html.options[i].value)
+            regionSelect_html.options[i].selected = true;
+            onchange()
+              break;
+          }
+      }
+    
+    }
+
+    // #endregion
+
     ///////////////////////////////////////////////////////////////////////////
     ////////////////////////// Draw the legend ////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
+
+    // #region
 
     //Color Legend container
     var legendsvg = svg.append("g")
@@ -135,15 +180,16 @@ d3.json("topo.json", function (error, us) {
       .attr("height", 20)
       .style("fill", "url(#linear-gradient)")
 
-      var colorScale = d3.scaleLinear()
+    var colorScale = d3.scaleLinear()
       // .domain([310575, 1893765]) 
-      .domain([310575,  1893765])
+      .domain([310575, 1893765])
       .range([0, 300])
 
     //Define x-axis
     xAxis = d3.axisBottom(colorScale)
-    .tickFormat(function(d){
-      return d/100000})
+      .tickFormat(function (d) {
+        return d / 100000
+      })
 
     //Set up X axis
     legendsvg.append("g")
@@ -151,59 +197,68 @@ d3.json("topo.json", function (error, us) {
       .attr("transform", "translate(0," + (20) + ")")
       .call(xAxis);
 
-
-     ///////////////////////////////////////////////////////////////////////////
+    // #endregion
+    ///////////////////////////////////////////////////////////////////////////
     ////////////////////////// Labels /////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-      var labels = svg.append("g").attr("class", "label-group");
-      labels.selectAll(".label")
-        .data(features)
-        .enter()
-        .append("text")
-        .attr("class", "label")
-        .attr("x", function(d) {
-          return path.centroid(d)[0];
-        })
-        .attr("y", function(d) {
-          return path.centroid(d)[1];
-        })
-        .text(function(d) {
+    // #region
+
+    var labels = svg.append("g").attr("class", "label-group");
+    labels.selectAll(".label")
+      .data(features)
+      .enter()
+      .append("text")
+      .attr("class", "label")
+      .attr("x", function (d) {
+        return path.centroid(d)[0];
+      })
+      .attr("y", function (d) {
+        return path.centroid(d)[1];
+      })
+      .text(function (d) {
           return d.properties.regionName;
-        });
+      });
+
+      // #endregion
 
     ///////////////////////////////////////////////////////////////////////////
     ////////////////////////// Select Logic ////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
-    d3.select('#regionSelect')
-      .on('change',onchange)
     
-     ///////////////////////////////////////////////////////////////////////////
+
+    regionSelect
+      .on('change', onchange)
+
+    ///////////////////////////////////////////////////////////////////////////
     ////////////////////////// Explanation Logic ////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-      function onchange() {
-        selectValue = d3.select('select').property('value')
-        sentence = selectValue + ' has a total undergraduate population of ' + formatComma(popObject[selectValue]) + '. '
-        sentence += 'My favorite country, The United States of America, has a total population of ' + formatComma(total) + '. '
-        sentence += 'Math tells us that ' + selectValue + ' makes up ' + formatDecimal(popObject[selectValue]/total*100) + '% of \'Merica\'s undergrad population'
-        d3.select('#explanation1')
-          .text(sentence)
-          // console.log(total)
-          // .text(selectValue)
+    function onchange() {
+      selectValue = regionSelect.property('value')
+      sentence = selectValue + ' has a total undergraduate population of ' + formatComma(popObject[selectValue]) + '. '
+      sentence += 'My favorite country, The United States of America, has a total undergrad population of ' + formatComma(total) + '. '
+      sentence += 'Math tells us that ' + selectValue + ' makes up ' + formatDecimal(popObject[selectValue] / total * 100) + '% of \'Merica\'s undergrad population'
+      
+      d3.select('#explanation1')
+        .text(sentence)
+      
+        //select the region which the dropdown selects
+      console.log(d3.select('#'+selectValue))
+      console.log(selectValue)
+      
 
-      };
+      d3.select('.selectedRegion').classed('selectedRegion', false);
+      d3.select('#'+(selectValue).replace(/\s/g, "")).classed('selectedRegion', true);
+    
+      
+    };
 
-      function doCalcs(selectVal) {
-            
-
-      }
-
+    selectValue = regionSelect.property('value')
+    if (selectValue !== '') {
+      console.log('going to onchange')
       onchange()
-
-    // console.log(regionSelect)
-    // var selectedRegion = regionSelect.options[regionSelect.selectedIndex].value
-    // console.log(selectedRegion)
+    }
 
   })
 
